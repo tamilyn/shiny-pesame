fe18ef2c825251cc0e74804ea69dbd1f59b24663
Tami Crawford <tamicrawford@Tamis-MBP-2.wlan.musc.edu>
2018-01-23 15:11:33 -0500

    checkpoint

--- shinypesamer/inst/example/fileImporterModule.R
+++ shinypesamer/inst/example/fileImporterModule.R
@@ -4,14 +4,20 @@ library(stringr)
 library(readr) 
 
 # module UI ----
+
+# depending on file type (csv, xlsx)
 importFileUI <- function(id) {
   ns <- NS(id)
-  tagList(uiOutput(ns("controls")))
+
+  tagList(h1("import file ui"), 
+	  uiOutput(ns("controls")))
 }
 
+# Display table 
 importDisplayFileUI <- function(id){
   ns <- NS(id)
-  tagList(uiOutput(ns("importDataTable")))
+  tagList(h1("import display filetable"), 
+	  uiOutput(ns("importDataTable")))
 }
 
 #####
@@ -55,25 +61,38 @@ fileImporterFile <- function(input, output, session, fi, fileOptions ) {
 
   ## use renderUI to display table
   output$table <- renderDataTable({
+     #ns <- session$ns
+     # dependencies - depending on type
+     flog.info("output$table")
      d <- dataframe()
+     flog.info("output$table", nrow(d))
      DT::datatable(d, options = list(pageLength = 5),
         caption = "Data Set Features", escape = FALSE)
  })
 
+#  mytable <- reactive({
+#     ext <- extension()
+#     return(ext == "xlsx" || ext == "csv" ? "table")
+#        tbl <- dataTableOutput(ns("table"))
+#  }
   output$importDataTable <- renderUI({
+    flog.info("importDataTable: renderUI")
+    ns <- session$ns
     tbl <- ""
     if(!is.null(userFile())) {
       ext <- extension()
       if( ext == "xlsx") {
         tbl <- dataTableOutput(ns("table"))
       } else if( ext == "csv") {
-        tbl <- dataTableOutput(ns("table"))
+	dataTableOutput(ns("table"))
       }
     }
 
     verticalLayout(
       textOutput(ns("description")),
-      tbl)
+      h4("before tbl"),
+      dataTableOutput(ns("table")),
+      h4("after tbl"))
   })
 
   ##--
@@ -92,17 +111,20 @@ fileImporterFile <- function(input, output, session, fi, fileOptions ) {
   csvControls <- reactive({
     tagList(
       fileInput(ns("file"), 'File'),
+      checkboxInput(ns("row_names"), "Use row names"),
       checkboxInput(ns("col_names"), "Use column names"),
-      checkboxInput(ns("row_names"), "Use row names"))
+      checkboxInput(ns("samplesAreRows"), "Samples are rows", value = TRUE))
   })
 
   output$controls <- renderUI({
+    ns <- session$ns
     file <- userFile()
     ext <- extension()
 
     if( !is.null(userFile()) ) {
       #if(ext == "xlsx") return(excelControls())
       if(ext == "RData") return(rdataControls())
+
       if(ext == "rds") return(rdsControls())
       if(ext == "csv") return(csvControls())
     }
@@ -118,34 +140,57 @@ fileImporterFile <- function(input, output, session, fi, fileOptions ) {
       fileInput(ns("file"), 'File'),
 
       if( !is.null(sheets))  
-           checkboxInput(ns("col_names"), "Use column names"),
-
-      if( !is.null(sheets))  
-           checkboxInput(ns("row_names"), "Use row names"),
-
-      if( !is.null(sheets))  
-           selectInput(ns("selectedSheet"), "Select Sheet",
+        tagList(
+          checkboxInput(ns("col_names"), "Use column names"),
+          checkboxInput(ns("row_names"), "Use row names"),
+          selectInput(ns("selectedSheet"), "Select Sheet",
              sheets, multiple = FALSE),
+          checkboxInput(ns("samplesAreRows"), "Samples are rows", value = TRUE),
+          fluidRow( column(6,
+            numericInput(ns("startRow"), "Start Row:", 1, min = 1, max = 9900)),
+            column(6,
+            numericInput(ns("startCol"), "Start Column:", 1, min = 1, max = 1000))),
 
-      if( !is.null(sheets))  
-           checkboxInput(ns("samplesAreRows"), "Samples are rows", value = TRUE),
-      
-      if( !is.null(sheets))  
-      fluidRow(
-      column(6,
-       numericInput(ns("startRow"), "Start Row:", 1, min = 1, max = 9900)),
-      column(6,
-       numericInput(ns("startCol"), "Start Column:", 1, min = 1, max = 1000))),
-
-
-      if( !is.null(sheets))  
-      fluidRow(
-      column(6,
-       numericInput(ns("endRow"), "End Row:", 1, min = 1, max = 9900)),
-      column(6,
-       numericInput(ns("endCol"), "EndColumn:", 1, min = 1, max = 1000)))
-     )
+          fluidRow( column(6,
+            numericInput(ns("endRow"), "End Row:", 1, min = 1, max = 9900)),
+            column(6,
+            numericInput(ns("endCol"), "EndColumn:", 1, min = 1, max = 1000))))) 
   })
+
+  readCsvData <- reactive({
+      d <- read_csv(userFile()$datapath, col_names = input$col_names)
+
+      # if row_names is selected, row names must be unique with no NA
+      if(input$row_names) {
+         rnames <- unlist(d[, 1])
+         has_na <- sum(is.na(rnames))
+         df <- data.frame(d[ , 2:ncol(d)])
+
+         if (has_na == 0 && length(unique(rnames))  == length(rnames)) {
+           flog.info(str_c("using row names, they are unique"))
+           rownames(df) <- rnames
+           d <- df
+         } else {
+           flog.error(str_c("195 can't use row names, is.na and/or not unique"))
+           warning("can't use row names, not unique")
+           d
+         }
+      }
+      flog.info(str_c("checking for samples are rows"))
+
+      # if samples are rows, then transpose
+      if("samplesAreRows" %in% names(input)&& input$samplesAreRows) {
+         flog.info(str_c("YES checking for samples are rows ", input$samplesAreRows ))
+         d <- t(d)
+         flog.info(str_c("===>  transpolosed", d))
+         return(d)
+      }
+      flog.info(str_c("NO checking for samples are rows", input$samplesAreRows))
+      flog.info(str_c("RETURNING checking for samples are rows ",
+                         str_c(str(d,1), collapse = " ")))
+      return(d)
+  })
+
 
   rawDataForSheet <- reactive({
     #this will be based on the type of extension
@@ -189,6 +234,7 @@ fileImporterFile <- function(input, output, session, fi, fileOptions ) {
            }
          }
     } else if(ext == "csv") {
+
       d <- read_csv(userFile()$datapath, col_names = input$col_names)
       if(input$row_names) {
          rnames <- unlist(d[, 1])
@@ -199,11 +245,26 @@ fileImporterFile <- function(input, output, session, fi, fileOptions ) {
            rownames(df) <- rnames
            d <- df
          } else {
-           flog.error(str_c("can't use row names, is.na and/or not unique"))
+           flog.error(str_c("195 can't use row names, is.na and/or not unique"))
            warning("can't use row names, not unique")
            d
          }
       }
+      flog.info(str_c("checking for samples are rows"))
+
+      # if samples are rows, then transpose
+      if("samplesAreRows" %in% names(input)) {
+         flog.info(str_c("YES checking for samples are rows ", input$samplesAreRows ))
+         d <- t(d)
+         flog.info(str_c("YES checking for samples are rows ",
+                         str_c(str(d,1), collapse = " ")))
+         return(d)
+      }
+      flog.info(str_c("NO checking for samples are rows", input$samplesAreRows))
+      flog.info(str_c("RETURNING checking for samples are rows ",
+                         str_c(str(d,1), collapse = " ")))
+      return(d)
+
 
     } else if(ext == "rdata") {
       d <- data_frame(A=1:4)
@@ -301,20 +362,21 @@ fileImporterFile <- function(input, output, session, fi, fileOptions ) {
 
   dataframe <- reactive({
     ext <- extension()
-    #print(str_c("DATA FRAME EXIST IS ", ext))
+    print(str_c("dataframe() DATA FRAME EXIST IS ", ext))
     if(is.null(ext)) return(NULL)
 
     if(extension() == "xlsx") {
       dlist <- dataForExcel()
       dlist[["dataframe_data"]]
     } else if(extension() == "csv") {
-      rawDataForSheet()
+      readCsvData()
     } else {
       print("data frame NOT XLSX or csv")
       return(NULL)
     }
   })
 
+  # observe toggle of samplesAreRows...
 
   # We can run observers in here if we want to
   observe({
