f712be896ecc695a0c452039b0d3a2cb5dba6c24
Tami Crawford <tami.crawford@gmail.com>
2018-01-26 15:40:00 -0500

    upload analyze tab, issue #7

--- shinypesamer/inst/example/ui.R
+++ shinypesamer/inst/example/ui.R
@@ -2,7 +2,28 @@
 library(shiny)
 library(bsplus)
 
-source("analyze.R")
+analyze_panel <- tagList(
+  div(id="analyze_no_data", h1("No available factors")),
+  hidden(div(id="analyze_has_data", 
+    fluidRow(
+      column(4, selectInput("adj_method",
+		label = "Comparison Method", choices = method.options,
+		selectize = FALSE,
+		size = 1,
+      selected = method.options.default)),
+      column(4, selectInput(inputId = "signficance_threshold",
+		label= "Significance Threshold",
+                selectize = FALSE,
+		size = 1,
+		choices = significance.options,
+                selected = significance.options.default )) ,
+      column(4,  uiOutput("factorVariables") )),
+
+    tabsetPanel(type="tabs",
+      tabPanel("Plot", plotlyOutput("filteredPlotly")),
+      tabPanel("Table", dataTableOutput("computedDataTable")) 
+    ))))
+
 
 top_part <- tags$nav(class="navbar navbar-default navbar-fixed-top",
   div(class="container",
